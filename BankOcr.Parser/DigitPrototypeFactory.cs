94134a35a0a40b8009340b4f150f058d24001a29
Wojciech Sroczynski <koda@koda.net.pl>
2022-10-19 12:19:40 +0200

    Initial case, project structure, first UT

---
+++ BankOcr.Parser/DigitPrototypeFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace BankOcr.Parser;
+
+public static class DigitPrototypeFactory
+{
+    public static IEnumerable<DigitPrototype> BuildPrototypes()
+    {
+        return
+            @"
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|"
+                .EnumerateGlyphs()
+                .Select((g, i) =>
+                    new DigitPrototype(
+                        Digit: i,
+                        Glyph: g,
+                        NumElems: g.Count(c => !char.IsWhiteSpace(c))));
+    }
+}
---
+++ BankOcr.Parser/GlyphEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace BankOcr.Parser;
+
+public static class GlyphEnumerator
+{
+    private const int NumCharsPerGlyph = 9;
+    private const int NumCharsPerGlyphPerLine = 3;
+    public static IEnumerable<string> EnumerateGlyphs(this string input)
+    {
+        var inputSanitized = input.ReplaceLineEndings("");
+        var length = inputSanitized.Length;
+        
+        Debug.Assert(length % NumCharsPerGlyph == 0); //TODO: improve with result?
+        var numGlyphs = length / NumCharsPerGlyph;
+
+        //TODO: improve with explicit key selector
+        return inputSanitized
+            .Select((c, i) => new { Character = c, Position = i })
+            .GroupBy(g => g.Position / NumCharsPerGlyphPerLine)
+            .Select((line, i) => new {Line = line.Select(l => l.Character), Digit = i % numGlyphs})
+            .GroupBy(elem => elem.Digit)
+            .Select(g => new string(g.SelectMany(v => v.Line).ToArray()));
+    }
+}
