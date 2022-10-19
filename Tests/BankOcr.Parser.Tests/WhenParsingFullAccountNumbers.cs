4253f0c1149e94b534e52fb2549e6c93af1c753c
Wojciech Sroczynski <koda@koda.net.pl>
2022-10-19 13:02:39 +0200

    Parsing full account numbers

---
+++ Tests/BankOcr.Parser.Tests/WhenParsingFullAccountNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BankOcr.Parser.Tests;
+
+[TestFixture]
+public class WhenParsingFullAccountNumbers
+{
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|", "000000000")]
+    [TestCase(@"
+                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |", "111111111")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+|_ |_ |_ |_ |_ |_ |_ |_ |_ ", "222222222")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+ _| _| _| _| _| _| _| _| _|", "333333333")]
+    [TestCase(@"
+                           
+|_||_||_||_||_||_||_||_||_|
+  |  |  |  |  |  |  |  |  |", "444444444")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+ _| _| _| _| _| _| _| _| _|", "555555555")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+|_||_||_||_||_||_||_||_||_|", "666666666")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |", "777777777")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+|_||_||_||_||_||_||_||_||_|", "888888888")]
+    [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+ _| _| _| _| _| _| _| _| _|", "999999999")]
+    [TestCase(@"
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|", "123456789")]
+    
+    public void ShouldParseTheNumber(string input, string expectedResult)
+    {
+        var prototypes = DigitPrototypeFactory.BuildPrototypes().ToArray();
+        var digits = input.EnumerateGlyphs();
+        var accountNumber = digits.Select(d => prototypes.First(p => d == p.Glyph));
+        var accountNumText = accountNumber.Aggregate(new StringBuilder(), (state, d) =>
+        {
+            state.Append(d.Digit.ToString());
+            return state;
+        });
+        Assert.AreEqual(expectedResult, accountNumText.ToString());
+    }
+}
--- Tests/BankOcr.Parser.Tests/WhenParsingSingleDigits.cs
+++ Tests/BankOcr.Parser.Tests/WhenParsingSingleDigits.cs
@@ -1,57 +1,55 @@
 using System.Linq;
 using NUnit.Framework;
 
-namespace BankOcr.Parser.Tests
+namespace BankOcr.Parser.Tests;
+
+[TestFixture]
+public class WhenParsingSingleDigits
 {
-    [TestFixture]
-    public class WhenParsingSingleDigits
-    { 
-[TestCase(0, @"
+    [TestCase(0, @"
  _ 
 | |
 |_|")]
-[TestCase(1,@"
+    [TestCase(1, @"
    
   |
   |")]
-[TestCase(2, @"
+    [TestCase(2, @"
  _ 
  _|
 |_ ")]
-[TestCase(3, @"
+    [TestCase(3, @"
  _ 
  _|
  _|")]
-[TestCase(4, @"
+    [TestCase(4, @"
    
 |_|
   |")]
-[TestCase(5, @"
+    [TestCase(5, @"
  _ 
 |_ 
  _|")]
-[TestCase(6, @"
+    [TestCase(6, @"
  _ 
 |_ 
 |_|")]
-[TestCase(7, @"
+    [TestCase(7, @"
  _ 
   |
   |")]
-[TestCase(8, @"
+    [TestCase(8, @"
  _ 
 |_|
 |_|")]
-[TestCase(9, @"
+    [TestCase(9, @"
  _ 
 |_|
  _|")]
-        
-        public void ShouldRecognizeDigit(int expectedDigit, string input)
-        {
-            var prototypes = DigitPrototypeFactory.BuildPrototypes().ToArray();
-            var matchingDigit = prototypes.First(d => d.Glyph == input.ReplaceLineEndings(""));
-            Assert.AreEqual(expectedDigit, matchingDigit.Digit);
-        }
+    public void ShouldRecognizeDigit(int expectedDigit, string input)
+    {
+        var prototypes = DigitPrototypeFactory.BuildPrototypes().ToArray();
+        var matchingDigit = prototypes.First(d => d.Glyph == input.ReplaceLineEndings(""));
+        Assert.AreEqual(expectedDigit, matchingDigit.Digit);
     }
 }
